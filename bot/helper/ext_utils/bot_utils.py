d0f331b0ee9a80bcf00eb7ac53b20ff6504b9191
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-14 19:14:40 +0500

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -169,7 +169,7 @@ T: {disk_t} | F: {disk_f}
 Working For: {currentTime}
 T-DL: {recv} | T-UL: {sent}
 
-Made with ❤️ by Dawn
+Made with ❤️ by Ibrahim
 """
 
 #----Thanks for deleting my name ❤️ Appreciate it----#
